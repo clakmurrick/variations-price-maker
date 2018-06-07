a12f6492a38790600a3a93cb0c58c5f7e389290b
Clark Merrick <clarkm74@gmail.com>
2018-06-07 11:26:18 -0500

    inital commit

---
+++ main.swift
@@ -0,0 +1,25 @@
+// Variations-Price-Maker
+// Updated 2018.06.07
+
+import Foundation
+
+let line:String = readLine()!
+
+print("Enter number of attributes")
+
+if let input = readLine() {
+  if let numOfAttributes = Int(input)
+}
+
+var totalVariations:String
+for _ in 0..<numOfAttributes {
+	let input:Int = Int(readLine())!
+
+	if let input = readLine() {
+  	if let numberOfAttributes = Int(input)
+	}		
+	
+	totalVariations *= input
+}
+
+print(totalVariations)
